63d133b1bb34448ce901332ee7ea1ee2da697835
Abdurahim <kalonov1890@gmail.com>
2022-10-26 18:33:38 -0400

    Part 2 ready code with question

--- src/CodingBatLogic1/Part2/lastDigit.java
+++ src/CodingBatLogic1/Part2/lastDigit.java
@@ -2,6 +2,12 @@ package CodingBatLogic1.Part2;
 
 public class lastDigit {
 
+
+    /*
+    Given three ints, a b c, return true if two or more of them have the same rightmost digit.
+    The ints are non-negative. Note: the % "mod" operator computes the remainder, e.g. 17 % 10 is 7.
+     */
+
     public boolean lastDigit(int a, int b, int c) {
 
         a = a % 10;
